c7fe3c4dc57b8e17149e67c7b08f8d7dcba84cd5
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-16 12:29:51 +0000

    Added OrderDetails model to catalog. closes #10

---
+++ catalogue/src/model/orderDetails.js
@@ -0,0 +1,31 @@
+// Created by David Walshe on 16/02/2020
+
+// NPM imports
+const {DataTypes} = require("sequelize");
+// Local imports
+const {sequelize, testConnection} = require("./../database/sequelize");
+
+// Test connection to db.
+testConnection("OrderDetails Model");
+
+// Define data model for OrderDetails table.
+const OrderDetails = sequelize.define("orderDetails", {
+    orderDetailsID: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    orderID: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+    },
+    productID: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    quantity: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    }
+});
