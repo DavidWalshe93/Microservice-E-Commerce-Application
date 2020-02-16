f02db276173eac15432a2abc968df87449e7e5f5
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-16 12:09:57 +0000

    Added sequelize Customer model

--- catalogue/src/model/customer.js
+++ catalogue/src/model/customer.js
@@ -1,2 +1,34 @@
 // Created by David Walshe on 16/02/2020
 
+// NPM imports
+const {DataTypes} = require("sequelize");
+// Local imports
+const {sequelize, testConnection} = require("./../database/sequelize");
+
+// Test connection to db.
+testConnection("Customer Model");
+
+// Define data model for Customer table.
+const Customer = sequelize.define("customer", {
+    customerID: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.STRING(60)
+    }
+}, {
+    timestamps: false
+});
+
+module.exports = Customer;
