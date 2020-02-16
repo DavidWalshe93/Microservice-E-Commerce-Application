04acfafc920e604d5f4fbced7e71e3f80942f964
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-16 12:52:53 +0000

    Added order router. closes #7

---
+++ catalogue/src/routers/order.js
@@ -0,0 +1,23 @@
+// Created by David Walshe on 16/02/2020
+
+// NPM Imports
+const express = require("express");
+// Local imports
+const Order = require("./../model/order");
+
+// Init express endpoint router
+const router = new express.Router();
+
+// Get all order items
+router.get(["/getOrders"], async (req, res) => {
+    try {
+        const orders = await Order.findAll()
+            .map(o => o.get({
+                plain: true
+            }));
+
+        return res.status(200).send(orders)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+});
