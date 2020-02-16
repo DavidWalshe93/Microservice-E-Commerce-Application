da9c96870a5fbaf94d8be6337121538b9d01d361
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-16 15:57:01 +0000

    Updated test suites to include orderDetails and customer creation.

--- catalogue/tests/fixtures/db_setup.js
+++ catalogue/tests/fixtures/db_setup.js
@@ -4,6 +4,8 @@
 const {sequelize} = require("../../src/database/sequelize");
 const Product = require("../../src/model/product");
 const Order = require("../../src/model/order");
+const OrderDetails = require("../../src/model/orderDetails");
+const Customer = require("../../src/model/customer");
 
 // ==================================================================================
 // PRODUCT SETUP
@@ -63,6 +65,65 @@ const setupOrderTable = async () => {
     }
 };
 
+
+// ==================================================================================
+// ORDER DETAILS SETUP
+// ==================================================================================
+
+var mock_order_details = [];
+const mock_order_details_generator = (size = 3) => {
+    mock_order_details = [];
+    for (let i = 1; i < size + 1; i++) {
+        mock_order_details.push({
+            orderID: i,
+            productID: i,
+            quantity: i
+        });
+    }
+
+    return mock_order_details;
+};
+
+const setupOrderDetailsTable = async () => {
+    await OrderDetails.sync();
+    await OrderDetails.destroy({
+        truncate: true
+    });
+    const mocks = mock_order_details_generator();
+    for (let i = 0; i < mocks.length; i++) {
+        await OrderDetails.create(mocks[i]);
+    }
+};
+
+// ==================================================================================
+// CUS SETUP
+// ==================================================================================
+
+var mock_customer_details = [];
+const mock_customer_generator = (size = 3) => {
+    mock_customer_details = [];
+    for (let i = 1; i < size + 1; i++) {
+        mock_customer_details.push({
+            name: "John" + 1,
+            password: "password" + 1,
+            address: `Apartment ${i}, 700 South Mall, Cork`
+        });
+    }
+
+    return mock_customer_details;
+};
+
+const setupCustomerTable = async () => {
+    await Customer.sync();
+    await Customer.destroy({
+        truncate: true
+    });
+    const mocks = mock_customer_generator();
+    for (let i = 0; i < mocks.length; i++) {
+        await Customer.create(mocks[i]);
+    }
+};
+
 // ==================================================================================
 // Database SETUP
 // ==================================================================================
@@ -70,6 +131,8 @@ const setupOrderTable = async () => {
 const setupDatabase = async () => {
     await setupProductTable();
     await setupOrderTable();
+    await setupOrderDetailsTable();
+    await setupCustomerTable();
 };
 
 module.exports = {setupDatabase, mock_products, mock_orders};
--- catalogue/tests/orderDetails.test.js
+++ catalogue/tests/orderDetails.test.js
@@ -1,2 +1,21 @@
 // Created by David Walshe on 16/02/2020
 
+// NPM imports
+const request = require("supertest");
+// Local imports
+const app = require("../src/app");
+const OrderDetails = require("../src/model/orderDetails");
+
+const {setupDatabase} = require("./fixtures/db_setup");
+
+beforeEach(setupDatabase);
+
+
+test("Should return all orderDetails", async () => {
+    const response = await request(app)
+        .get("/getOrderDetails")
+        .send()
+        .expect(200);
+
+    console.log(response)
+});
