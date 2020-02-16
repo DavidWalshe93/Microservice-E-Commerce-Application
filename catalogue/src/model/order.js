43829db9b9cc07d5f0f4d0478490f13842f0a6c1
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-16 12:22:49 +0000

    Added Order model to catalog. Closes #9

--- catalogue/src/model/order.js
+++ catalogue/src/model/order.js
@@ -4,3 +4,28 @@
 const {DataTypes} = require("sequelize");
 // Local imports
 const {sequelize, testConnection} = require("./../database/sequelize");
+
+// Test connection to db.
+testConnection("Order Model");
+
+// Define data model for Order table.
+const Order = sequelize.define("order", {
+    orderID: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    customerID: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    saledate: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    }
+}, {
+    timestamps: false
+});
+
+module.exports = Order;
