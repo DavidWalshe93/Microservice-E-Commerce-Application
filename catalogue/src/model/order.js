07662050e4af83b9c39921ba5fe8c859b9d52c2f
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-16 12:14:45 +0000

    Added empty Order model to catalog.

---
+++ catalogue/src/model/order.js
@@ -0,0 +1,6 @@
+// Created by David Walshe on 16/02/2020
+
+// NPM imports
+const {DataTypes} = require("sequelize");
+// Local imports
+const {sequelize, testConnection} = require("./../database/sequelize");
