7762dea35249046d411601ad28cf6ea2c1e8b85a
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-19 23:34:52 +0000

    Added new route handler for /add path. Items can now be added to the cart or updated if already existing.

--- cart service/src/routers/item.js
+++ cart service/src/routers/item.js
@@ -7,8 +7,41 @@ const Item = require("./../model/item");
 // Create a Express router
 const router = express.Router();
 
-router.post("/add", (req, res) => {
-    // TODO Add new data to in memory database here.
+router.post("/add", async (req, res) => {
+    const item = req.body;
+    try {
+        // Check if the item exists in the database already
+        const existingItem = await Item.findOne({
+            where: {
+                productID: item.productID,
+                customerID: item.customerID
+            }
+        });
+
+        // If no existing items match, add the new item to the cart
+        if (!existingItem) {
+            const newItem = await Item.create(item);
+
+            return res.status(201).send(newItem);
+        } else {
+            // Else get the matching row and update it.
+            existingItem.quantity += item.quantity;
+            existingItem.totalPrice = existingItem.price * existingItem.quantity;
+
+            // Commit the row updates.
+            await Item.update({
+                quantity: existingItem.quantity,
+                totalPrice: existingItem.price * existingItem.quantity
+            }, {
+                where: {itemID: existingItem.itemID}
+            });
+
+            return res.status(202).send(existingItem)
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
+    }
 });
 
 router.delete("/cart/:custID/items/:id", (req, res, next) => {
