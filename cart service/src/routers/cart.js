55cd21db9bdb924b405fe8adfc6041b5ac6022cf
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-19 21:27:16 +0000

    Added new router, cart.js. Paths are left as TODO until model layer is finalised.

---
+++ cart service/src/routers/cart.js
@@ -0,0 +1,22 @@
+// Created by David Walshe on 19/02/2020
+
+// NPM modules
+const express = require("express");
+
+// Create a Express router
+const router = express.Router();
+
+router.post("/add", (req, res) => {
+    // TODO Add new data to in memory database here.
+});
+
+router.delete("/cart/:custID/items/:id", (req, res, next) => {
+    // Todo Remove an item from the cart database
+});
+
+router.get("/cart/:custId/items", (req, res, next) => {
+    // Todo get the item list for the customer.
+});
+
+// Export router for external usage.
+module.exports = router;
