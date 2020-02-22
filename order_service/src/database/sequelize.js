c0051432f98c00d36864c42a2790d95dec7a2572
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-22 19:08:58 +0000

    Added sequelize.js for ORM usage with SQLite

---
+++ order_service/src/database/sequelize.js
@@ -0,0 +1,39 @@
+// Created by David Walshe on 22/02/2020
+
+// NPM imports
+const sqlite = require("sqlite3");
+const {Sequelize} = require("sequelize");
+
+// Get DB path
+const DATABASE_PATH = process.env.SQLITE_DB_PATH;
+
+// Create database
+const db = new sqlite.Database(DATABASE_PATH);
+// Connect sequelize instance to SQLite DB.
+const sequelize = new Sequelize("database", "", "", {
+        dialect: "sqlite",
+        storage: DATABASE_PATH,
+        logging: process.env.SQL_LOGGER | console.log,
+    }
+);
+
+const testConnection = async (msg) => {
+    try {
+        // TODO implement a logger here.
+        await sequelize.authenticate();
+        console.log("SUCCESS", msg);
+    } catch (e) {
+        console.log("FAILURE", msg, e);
+    }
+};
+
+// Sync database to Sequelize ORM
+const syncDatabase = async () => {
+    try {
+        await sequelize.sync()
+    } catch (e) {
+        console.error("Could not sync database with model")
+    }
+};
+
+module.exports = {sequelize, testConnection, syncDatabase};
