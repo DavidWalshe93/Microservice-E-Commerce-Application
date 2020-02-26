200bff64e5efbe4aabcd726a42a28b41717eb8ea
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-26 22:30:36 +0000

    Added Formik for form validation in React for RegisterPage

--- front_end_react/src/components/RegisterPage.js
+++ front_end_react/src/components/RegisterPage.js
@@ -2,11 +2,34 @@
 
 // npm imports
 import React from "react";
+import {useFormik} from "formik";
 
-const RegisterPage = () => (
-    <>
-        <p>Register Page</p>
-    </>
-);
+const RegisterPage = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            firstname: "",
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    });
+
+    return (
+        <>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor={"firstname"}>First Name</label>
+                <input
+                    id={"firstname"}
+                    name={"firstname"}
+                    type={"firstname"}
+                    onChange={formik.handleChange}
+                    value={formik.values.firstname}
+                />
+                <button type={"submit"}>Submit</button>
+            </form>
+        </>
+    )
+};
 
 export default RegisterPage;
