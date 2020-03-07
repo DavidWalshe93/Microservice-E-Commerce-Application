f0ca1e2ed3cedba6304ba5bf0eee37b4e10f3150
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-03-07 11:41:00 +0000

    Form layout complete.

--- front_end_react/src/components/LoginPage.js
+++ front_end_react/src/components/LoginPage.js
@@ -3,20 +3,46 @@
 // npm imports
 import React from "react";
 import {Form, Button, Col} from "react-bootstrap"
+import TextField from "./RegisterForm/TextField";
 
+// local imports
+import LoginValidator from "../validators/loginValidator";
+import "../styles/styles.scss"
 
 const LoginPage = () => {
 
     const formik = LoginValidator();
 
     const fieldWidth = 3;
-    const fieldOffset = 3;
-
+    const fieldOffset = 4;
 
     return (
         <>
             <Form noValidate onSumbit={formik.handleSubmit}>
-
+                <Form.Row>
+                    <Form.Group as={Col} md={{span: fieldWidth + 3, offset: fieldOffset}}>
+                        {/*Heading*/}
+                        <h4 className={"login-msg"}>Welcome back</h4>
+                    </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                    {/*Email*/}
+                    <Form.Group as={Col} md={{span: fieldWidth, offset: fieldOffset}} controlId={"email"}>
+                        <TextField name="email" label="Email" formik={formik} type={"email"}/>
+                    </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                    {/*Password*/}
+                    <Form.Group as={Col} md={{span: fieldWidth, offset: fieldOffset}} controlId={"password"}>
+                        <TextField name="password" label="Password" formik={formik} type={"password"}/>
+                    </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                    {/*Submit button*/}
+                    <Form.Group as={Col} md={{span: fieldWidth, offset: fieldOffset}} controlId={"submitRow"}>
+                        <Button className={"login-button"} type={"submit"}>Login</Button>
+                    </Form.Group>
+                </Form.Row>
             </Form>
         </>
     )
