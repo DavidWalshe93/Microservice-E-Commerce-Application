065e95c96b34290159b32c77627ecc49458d4ed6
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-03-08 19:50:51 +0000

    Added store to Redux Provider.

--- front_end_react/src/index.js
+++ front_end_react/src/index.js
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom';
 import AppRouter from "./routers/AppRouter";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from "react-redux";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
 
 const jsxRoot = (
-    <Provider>
+    <Provider store={store}>
         <AppRouter/>
     </Provider>
 );
