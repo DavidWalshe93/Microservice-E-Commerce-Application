47c7f20e9f6a666931ddb15f0f6f1737d096a6e2
DavidWalshe93 <david.walshe93@gmail.com>
2020-05-14 16:51:02 +0100

    Updated index.js for front end service.

--- front_end_service/src/index.js
+++ front_end_service/src/index.js
@@ -6,5 +6,10 @@ const app = require("./app");
 const port = process.env.PORT || 8079;
 
 app.listen(port, () => {
+    console.log("Registered Endpoints")
+    console.log("\t", process.env.USER_SERVICE_DEST, ":", process.env.USER_SERVICE_PORT)
+    console.log("\t",process.env.CATALOG_SERVICE_DEST, ":", process.env.CATALOG_SERVICE_PORT)
+    console.log("\t",process.env.CART_SERVICE_DEST, ":", process.env.CART_SERVICE_PORT)
+    console.log("\t",process.env.ORDER_SERVICE_DEST, ":", process.env.ORDER_SERVICE_PORT)
     console.log("Front-end Service is running on port", port)
 });
