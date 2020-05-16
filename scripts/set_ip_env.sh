3992d6475fa70931ada99836f0618775618f2fd7
David <david.walshe93@gmail.com>
2020-05-16 03:25:46 +0100

    Added printout to ip Script

--- scripts/set_ip_env.sh
+++ scripts/set_ip_env.sh
@@ -1,3 +1,4 @@
 # Creates an Environment variable for the linking services
 # Use "source" not "bash"
 export REACT_APP_HOSTNAME=$(hostname -I | awk '{print $1}')
+printenv | grep -i REACT_APP_HOSTNAME
